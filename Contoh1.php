1f90deac6b4c508fdb7e046b72fc88ace59ddb78
pq707 <rifkyfadillah66@gmail.com>
2022-03-17 17:22:05 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index ()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo "Nama saya siapa?
+		 	rumah saya ditinggal
+		 	Olahraga 5 jari";
+	}
+}
